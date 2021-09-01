327d3d8d11f207ddca38469b01ccb67a27e3c177
Matthew Lewis <matthew.knight.lewis@gmail.com>
2021-09-01 19:05:17 -0400

    money game

--- angular-src/src/app/profile/profile.component.ts
+++ angular-src/src/app/profile/profile.component.ts
@@ -10,15 +10,32 @@ import { UserService } from '../services/user.service';
 export class ProfileComponent implements OnInit {
 
   user: any
+  shopInventory: Map<string, any> = new Map()
 
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+
+    //Shop Defaults
+    this.shopInventory.set('gundarium', {name: 'gundarium', price: 5, description: 'lunar titanium alloy'})
+
     this.userService.getProfile().subscribe((res:any)=>{
       this.user = res.user
-    }, (err)=>{
+      setInterval(()=>{
+        this.user.money++;
+      }, 5000)
+    },
+    (err)=>{
       console.log('ERROR!')
       console.log(err)
     })
+  }
+
+  buy(shopItem: string) {
+    var item = this.shopInventory.get(shopItem)
+    if (item && item.price <= this.user.money) {
+      this.user.money -= item.price;
+      console.log(item)
+    }
   }
 }
